e2fabfbb62f08736fa58050586186da78a25ef55
fseasy <xuwei.fs@outlook.com>
2026-02-19 14:08:22 +0800

    update project init

--- deploy/project_init.sh
+++ deploy/project_init.sh
@@ -5,14 +5,10 @@ ENV_NAME="$ENV"
 PROJECT_ROOT_DIR=$PROJECT_ROOT_LOCAL_DIR
 PROD_CONF_REPO_DIR=$PROD_CONF_SYNC_GIT_REPO_LOCAL_DIR
 PROD_CONF_ORIGIN_PATH=$PROD_CONF_SYNC_REL_PATH
-REPO_PATH=$REPO_SSH_PATH
 NGINX_TGT_DIR=$NGINX_SYSTEM_CONF_DIR
 
 PROD_CONF_PROJECT_INSIDE_DIR="$PROJECT_ROOT_DIR/confgen/uni-conf/$ENV_NAME"
 
-# clone
-echo "CLONE repo to [$PROJECT_ROOT_DIR]"
-git clone $REPO_PATH $PROJECT_ROOT_DIR
 # prepare conf from another private repo: 
 # 1. enter the private repo to fetch the latest conf 2. link it to the project inside
 echo "pull the config file ${ENV_NAME}.py from ${PROD_CONF_REPO_DIR} git repo"
@@ -34,8 +30,12 @@ uv sync
 uv pip install -e .
 # 4. gen conf; link the nginx conf to the system nginx conf dir
 echo "Generate conf for $ENV_NAME"
-cd confgen
+cd  "$PROJECT_ROOT_DIR/confgen"
 uv run python gen.py -e $ENV_NAME
 echo "Link nginx conf"
 ln -s "$NGINX_TGT_DIR/docgate.conf" "$PROJECT_ROOT_DIR/nginx/${ENV_NAME}.conf"
+# 5. build vite
+echo "Vite build"
+cd  "$PROJECT_ROOT_DIR/frontend"
+pnpm run build
 echo "done"
